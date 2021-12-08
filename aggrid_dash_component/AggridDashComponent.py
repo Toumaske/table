e4e97e85aed86f5c18e8d86c59f167e5cae46748
krest <krest.toumas@outlook.de>
2021-12-08 12:24:50 +0100

    see documentation for more information !

--- aggrid_dash_component/AggridDashComponent.py
+++ aggrid_dash_component/AggridDashComponent.py
@@ -11,54 +11,72 @@ displays them.
 
 Keyword arguments:
 
-- id (string; optional)
+- id (string; optional):
+    The ID used to identify this component in Dash callbacks.
 
-- animateRows (boolean; optional)
+- animateRows (boolean; optional):
+    animateRows for ag-Grid.
 
-- cellValueChanged (list; optional)
+- columnDefs (list; optional):
+    columnDefs for ag-Grid.
 
-- columnDefs (list; optional)
+- columnState (list; optional):
+    columnState for ag-Grid.
 
-- columnState (list; optional)
+- debounceVerticalScrollbar (boolean; optional):
+    debounceVerticalScrollbar for ag-Grid.
 
-- debounceVerticalScrollbar (boolean; optional)
+- enableCellChangeFlash (boolean; optional):
+    enableCellChangeFlash for ag-Grid.
 
-- enableCellChangeFlash (boolean; optional)
+- gridApi (dict; optional):
+    gridApi for ag-Grid.
 
-- gridApi (dict; optional)
+- pagination (boolean; optional):
+    pagination for ag-Grid.
 
-- pagination (boolean; optional)
+- pinnedTopRowData (boolean | number | string | dict | list; optional):
+    pinnedTopRowData for ag-Grid.
 
-- pinnedTopRowData (boolean | number | string | dict | list; optional)
+- resizedColumn (list; optional):
+    resizedColumn for ag-Grid.
 
-- resizedColumn (list; optional)
+- rowBuffer (string; optional):
+    rowBuffer for ag-Grid.
 
-- rowBuffer (string; optional)
+- rowData (list; optional):
+    rowData for ag-Grid.
 
-- rowData (list; optional)
+- rowDragEntireRow (boolean; optional):
+    rowDragEntireRow for ag-Grid.
 
-- rowDragEntireRow (boolean; optional)
+- rowDragManaged (boolean; optional):
+    rowDragManaged for ag-Grid.
 
-- rowDragManaged (boolean; optional)
+- rowSelection (string; optional):
+    rowSelection for ag-grid.
 
-- rowSelection (string; optional)
+- selectedRows (list; optional):
+    selectedRows for ag-Grid.
 
-- selectedRows (list; optional)
+- singleClickEdit (boolean; optional):
+    singleClickEdit for ag-Grid.
 
-- singleClickEdit (boolean; optional)
+- table_height (string; optional):
+    table_height for ag-Grid.
 
-- table_height (string; optional)
+- table_width (string; optional):
+    table_width for ag-Grid.
 
-- table_width (string; optional)
-
-- tooltipShowDelay (string; optional)"""
+- tooltipShowDelay (string; optional):
+    tooltipShowDelay for ag-Grid."""
     @_explicitize_args
-    def __init__(self, id=Component.UNDEFINED, rowSelection=Component.UNDEFINED, gridApi=Component.UNDEFINED, columnDefs=Component.UNDEFINED, rowData=Component.UNDEFINED, selectedRows=Component.UNDEFINED, columnState=Component.UNDEFINED, resizedColumn=Component.UNDEFINED, cellValueChanged=Component.UNDEFINED, table_height=Component.UNDEFINED, table_width=Component.UNDEFINED, pinnedTopRowData=Component.UNDEFINED, pagination=Component.UNDEFINED, animateRows=Component.UNDEFINED, enableCellChangeFlash=Component.UNDEFINED, rowDragManaged=Component.UNDEFINED, rowDragEntireRow=Component.UNDEFINED, tooltipShowDelay=Component.UNDEFINED, singleClickEdit=Component.UNDEFINED, debounceVerticalScrollbar=Component.UNDEFINED, rowBuffer=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['id', 'animateRows', 'cellValueChanged', 'columnDefs', 'columnState', 'debounceVerticalScrollbar', 'enableCellChangeFlash', 'gridApi', 'pagination', 'pinnedTopRowData', 'resizedColumn', 'rowBuffer', 'rowData', 'rowDragEntireRow', 'rowDragManaged', 'rowSelection', 'selectedRows', 'singleClickEdit', 'table_height', 'table_width', 'tooltipShowDelay']
+    def __init__(self, id=Component.UNDEFINED, rowSelection=Component.UNDEFINED, gridApi=Component.UNDEFINED, columnDefs=Component.UNDEFINED, rowData=Component.UNDEFINED, selectedRows=Component.UNDEFINED, columnState=Component.UNDEFINED, resizedColumn=Component.UNDEFINED, table_height=Component.UNDEFINED, table_width=Component.UNDEFINED, pinnedTopRowData=Component.UNDEFINED, pagination=Component.UNDEFINED, animateRows=Component.UNDEFINED, enableCellChangeFlash=Component.UNDEFINED, rowDragManaged=Component.UNDEFINED, rowDragEntireRow=Component.UNDEFINED, tooltipShowDelay=Component.UNDEFINED, singleClickEdit=Component.UNDEFINED, debounceVerticalScrollbar=Component.UNDEFINED, rowBuffer=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'animateRows', 'columnDefs', 'columnState', 'debounceVerticalScrollbar', 'enableCellChangeFlash', 'gridApi', 'pagination', 'pinnedTopRowData', 'resizedColumn', 'rowBuffer', 'rowData', 'rowDragEntireRow', 'rowDragManaged', 'rowSelection', 'selectedRows', 'singleClickEdit', 'table_height', 'table_width', 'tooltipShowDelay']
         self._type = 'AggridDashComponent'
         self._namespace = 'aggrid_dash_component'
         self._valid_wildcard_attributes =            []
-        self.available_properties = ['id', 'animateRows', 'cellValueChanged', 'columnDefs', 'columnState', 'debounceVerticalScrollbar', 'enableCellChangeFlash', 'gridApi', 'pagination', 'pinnedTopRowData', 'resizedColumn', 'rowBuffer', 'rowData', 'rowDragEntireRow', 'rowDragManaged', 'rowSelection', 'selectedRows', 'singleClickEdit', 'table_height', 'table_width', 'tooltipShowDelay']
+        self.available_properties = ['id', 'animateRows', 'columnDefs', 'columnState', 'debounceVerticalScrollbar', 'enableCellChangeFlash', 'gridApi', 'pagination', 'pinnedTopRowData', 'resizedColumn', 'rowBuffer', 'rowData', 'rowDragEntireRow', 'rowDragManaged', 'rowSelection', 'selectedRows', 'singleClickEdit', 'table_height', 'table_width', 'tooltipShowDelay']
         self.available_wildcard_properties =            []
         _explicit_args = kwargs.pop('_explicit_args')
         _locals = locals()
